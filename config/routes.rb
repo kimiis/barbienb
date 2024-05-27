0275b332b5e5ec35e79f516f74caaef7a3743bed
kim isabelle <isabelle.kim2023@campus-eni.fr>
2024-05-27 16:59:52 +0200

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "houses#index"
-  resources :houses, only i%[new create show] do
-    resources :bookings, only i%[new create show validate]
+  resources :houses, only: %i[new create show] do
+    resources :bookings, only: %i[new create show]
   end
 end
