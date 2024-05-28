a6b8d4b1bb282b7e8a9ca5fe226485e75a9ca806
Juan Vianney NM <juanvianneynm@gmail.com>
2024-05-28 16:56:36 +0200

    remove house has been completed and roots links has been hadded

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "houses#index"
-  resources :houses, only: %i[new create show] do
+  resources :houses, only: %i[new create show destroy] do
     resources :bookings, only: %i[new create show]
   end
 end
