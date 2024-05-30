881c5c6025c570a0250b98a536a5dcddf24f041c
Juan Vianney NM <juanvianneynm@gmail.com>
2024-05-30 14:14:07 +0200

    reform the show

--- db/schema.rb
+++ db/schema.rb
@@ -74,14 +74,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_30_073614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.float "latitude"
-    t.float "longitude"
     t.integer "guest"
     t.integer "bedroom"
     t.integer "bed"
     t.integer "bath"
     t.integer "pool"
     t.boolean "pet"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["category_id"], name: "index_houses_on_category_id"
     t.index ["user_id"], name: "index_houses_on_user_id"
   end
