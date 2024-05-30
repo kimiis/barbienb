5af055d5c44f7aba64259f482aa5079ba9318c3b
Yoann Tourtellier <tourtellier.yoann@gmail.com>
2024-05-30 15:23:01 +0200

    amélioration du dashboard

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,19 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+
+
+document.getElementById('button1').onclick = function() {
+  document.getElementById('div1').classList.add('show');
+  document.getElementById('div2').classList.remove('show');
+  this.classList.add('active');
+    document.getElementById('button2').classList.remove('active')
+};
+
+document.getElementById('button2').onclick = function() {
+  this.classList.add('active');
+  document.getElementById('button1').classList.remove('active');
+  document.getElementById('div2').classList.add('show');
+  document.getElementById('div1').classList.remove('show');
+};
