e3fbc806a071bd3bcfe7c576161d036ef6f7d3e7
Yoann Tourtellier <tourtellier.yoann@gmail.com>
2024-05-31 12:24:06 +0200

    JS dashboard ok et add destroy sur card ma house

---
+++ app/javascript/controllers/dashboard_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+
+// Connects to data-controller="dashboard"
+export default class extends Controller {
+  static targets = ["housesContainer", "bookingsContainer", "bookingsBtn", "housesBtn"]
+  connect() {
+    console.log("coucou");
+  }
+  setActiveBookings() {
+    this.housesContainerTarget.classList.remove("show")
+    this.bookingsContainerTarget.classList.add("show")
+    this.bookingsBtnTarget.classList.add("active")
+    this.housesBtnTarget.classList.remove("active")
+  }
+  setActiveHouses() {
+    this.housesContainerTarget.classList.add("show")
+    this.bookingsContainerTarget.classList.remove("show")
+    this.housesBtnTarget.classList.add("active")
+    this.bookingsBtnTarget.classList.remove("active")
+  }
+}
